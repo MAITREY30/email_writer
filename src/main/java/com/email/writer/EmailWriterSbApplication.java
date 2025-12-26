7e04c82ee6952485ca4118471805f34115f42a2e
MAITREY30 <maitreyyadav8@gmail.com>
2025-12-26 23:37:46 +0530

    Initial commit

---
+++ src/main/java/com/email/writer/EmailWriterSbApplication.java
@@ -0,0 +1,20 @@
+package com.email.writer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmailWriterSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterSbApplication.class, args);
+	}
+
+
+    public static class EmailGeneratorController {
+
+    
+    }
+}
